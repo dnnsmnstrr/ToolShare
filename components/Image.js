3a17f2a85b499919b2c149bc6cf5435a6a8129b6
Dennis Muensterer <dennismuensterer@aol.com>
2021-06-30 10:44:37 +0200

    Fix missung api url in image

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Image, TouchableOpacity} from 'react-native'
+import useAuth from '../hooks/useAuth'
 const ToolImage = ({id, onPress, style, ...restProps}) => {
+  const {API_URL} = useAuth()
   const uri = API_URL + 'files/' + id
   return <TouchableOpacity onPress={onPress} activeOpacity={onPress ? 0.2 : 1} style={{ width: '100%', height: 200, ...style}}>
     <Image
